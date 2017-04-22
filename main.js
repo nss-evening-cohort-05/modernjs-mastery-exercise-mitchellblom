272b07851618204933590c1e28dc51cef995c1a9
Mitchell Blom <mitchellblom@gmail.com>
2017-04-22 10:56:15 -0500

    which team button clicked logging

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
 
-	giantArray = [];
+	let giantArray = [];
+
+	$(".team-selector").click((e)=> {
+		dataGetter(e)
+	});
 
     const loadTeams = () => {
         return new Promise((resolve, reject) => {
@@ -38,19 +42,71 @@ $(document).ready(() => {
         });
     };
 
-Promise.all([loadTeams(),loadGenders(), loadCharacters()])
-		.then(function(results){
-			results.forEach(function(ajaxCalls){
-				ajaxCalls.forEach(function(each){
-					giantArray.push(each);
+    const dataGetter = (e) => {
+    	giantArray = [];
+		Promise.all([loadTeams(),loadGenders(), loadCharacters()])
+				.then(function(results){
+					results.forEach(function(ajaxCalls){
+						ajaxCalls.forEach(function(each){
+							giantArray.push(each);
+						});
+					});
+							console.log("giantArray: ", giantArray);
+							console.log("which button pressed: ", $(e.currentTarget))
+							let whichButtonClicked = e.currentTarget
+					determineWhichToWrite(whichButtonClicked);
 				});
-			});
-					console.log("giantArray: ", giantArray);
+	}
 
-// 			writeDOM();
-		});
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 
+	const determineWhichToWrite = () => {
+		console.log("getting to determine");
 
+			// match team button clicked to team id.
+			// console.log the team id number
+			// write to dom only those characters who have a matching team_id
+
+				// 			writeProductCardsToDOM(giantArray);
+
+	}
+
+	const writeProductCardsToDOM = (array) => {
+
+            $("#character-container").html("");
+            characterString = "";
+            if (i % 4 === 0) {
+                characterString += `<div class="row">`;
+            }
+            for (var i = 0; i < array.length; i++) {
+
+								types.forEach(function(passedType){
+									if (passedType.id === array[i].type){
+										array[i].typeName = passedType.name;
+										array[i].categoryId = passedType.category_id;
+									}
+								});
+
+								categories.forEach(function(passedCategory){
+									if (passedCategory.id === array[i].categoryId){
+										array[i].categoryName = passedCategory.name;
+									}
+								});
+
+                characterString += `<div class="col-md-3 characterCard ${array[i].type}" id="${array[i].name}">
+                				<h3>${array[i].name}</h3>
+                				<img src="${array[i].image}" class="img-circle thumbnail" alt="Product Image">
+                				<h4>${array[i].description}</h4>
+                				<h4>Type: ${array[i].typeName}</h4>
+                				<h4>Category: ${array[i].categoryName}</h4>
+                				</div>`;
+
+                if (i % 4 === 3) {
+                    characterString += `</div>`;
+                }
+            }
+            $("#character-container").html(characterString);
+        }
 
 });
