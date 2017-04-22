bdd180e628ad34743d2613a25c5dd8cfcfbc122a
Mitchell Blom <mitchellblom@gmail.com>
2017-04-22 12:17:32 -0500

    marvel logo on navbar and stying to character cards

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
 
 	let giantArray = [];
+	let counter = [];
 
 	$(".team-selector").click((e)=> {
 		dataGetter(e)
@@ -52,7 +53,7 @@ $(document).ready(() => {
 						});
 					});
 							console.log("giantArray: ", giantArray);
-							let whichButtonClicked = e.currentTarget
+							let whichButtonClicked = e.currentTarget;
 					determineWhichToWrite(whichButtonClicked);
 				});
 	}
@@ -68,71 +69,36 @@ $(document).ready(() => {
 		let clickedTeam = whichButtonClicked.id;
 		giantArray.forEach((object) => {
 			if (object.name == clickedTeam){
-				console.log("match found and the id is: ", object.id);
 				matchingId = object.id;
-				matchingTeamName = object.name;
 			}
 		});
 		giantArray.forEach((object) => {
-				if (object.team_id == matchingId) {						
-					matchingCharacterObject = object;
-					writeCharacterToDom(matchingCharacterObject, clickedTeam);
+			if (object.team_id == matchingId) {						
+				matchingCharacterObject = object;
+				writeCharacterToDom(matchingCharacterObject, clickedTeam);
 		}
 		})
-
 	}
 
 	const writeCharacterToDom = (matchingCharacterObject, clickedTeam) => {
+		// add one to counter here, and reset counter somewhere to add `<div class="row">` and `</div>`
+
+		// if (i % 4 === 0) {
+  //               characterString += `<div class="row">`;
+  //           }
+
 		let characterString = `<div class="col-md-3 characterCard" id="${matchingCharacterObject.name}">
                 				<h3>${matchingCharacterObject.name}</h3>
-                				<img src="${matchingCharacterObject.image}" class="img-circle thumbnail" alt="Character Image">
+                				<img src="${matchingCharacterObject.image}" class="characterImage thumbnail" alt="Character Image">
                 				<h4>${matchingCharacterObject.description}</h4>
                 				<h4>Team: ${clickedTeam}</h4>
                 				<h4>Gender: ${matchingCharacterObject.gender_id}</h4>
                 				</div>`;
+            // if (i % 4 === 3) {
+            //         characterString += `</div>`;
+            //     }
             $("#character-container").append(characterString);
+
 	}
-
-
-
-
-
-
-	const writeProductCardsToDOM = (array) => {				// below similar to previous exercise. using for reference
-
-            $("#character-container").html("");
-            characterString = "";
-            if (i % 4 === 0) {
-                characterString += `<div class="row">`;
-            }
-            for (var i = 0; i < array.length; i++) {
-
-								types.forEach(function(passedType){
-									if (passedType.id === array[i].type){
-										array[i].typeName = passedType.name;
-										array[i].categoryId = passedType.category_id;
-									}
-								});
-
-								categories.forEach(function(passedCategory){
-									if (passedCategory.id === array[i].categoryId){
-										array[i].categoryName = passedCategory.name;
-									}
-								});
-
-                characterString += `<div class="col-md-3 characterCard ${array[i].type}" id="${array[i].name}">
-                				<h3>${array[i].name}</h3>
-                				<img src="${array[i].image}" class="img-circle thumbnail" alt="Product Image">
-                				<h4>${array[i].description}</h4>
-                				<h4>Type: ${array[i].typeName}</h4>
-                				<h4>Category: ${array[i].categoryName}</h4>
-                				</div>`;
-
-                if (i % 4 === 3) {
-                    characterString += `</div>`;
-                }
-            }
-            $("#character-container").html(characterString);
-        }
 
 });
